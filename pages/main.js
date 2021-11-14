0aa83bf92e5b39128af94ee7abb42b2af4d26ae3
vakulanikita <nik.vakula@bk.ru>
2021-11-14 12:52:00 +0300

    fix(export): tailwindcss

---
+++ pages/main.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-green-500">Hello, world!</h1>
+    </div>
+  )
+}
