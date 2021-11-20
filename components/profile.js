85862cf607349e2fe37ef76699fe485c5faf9b5e
vakulanikita <nik.vakula@bk.ru>
2021-11-20 11:21:06 +0300

    feat: profile component

---
+++ components/profile.js
@@ -0,0 +1,36 @@
+import {
+  Box,
+  Heading,
+  Text,
+  Image
+} from '@chakra-ui/react'
+
+export default function Profile() {
+  return (
+    <Box display={{md: 'flex'}}>
+      <Box flexGrow={1}>
+        <Heading as="h2">Nikita Vakula</Heading>
+        <Text>Digital Master ( Developer / Designer )</Text>
+      </Box>
+
+      <Box
+        flexShrink={0}
+        mt={{ base: 4, md: 0 }}
+        ml={{ md: 6 }}
+        textAlign="center"
+      >
+        <Image
+          display="inline-block"
+          borderColor="whiteAlpha.800"
+          borderWidth={2}
+          borderStyle="solid"
+          borderRadius="full" 
+          w="100px"
+          h="100px"
+          alt="" 
+          src="nikita.jpg"
+        />
+      </Box>
+    </Box>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Link from 'next/link';
 import Layout from '../components/layouts/main.js'
 import Alert from '../components/alert'
+import Profile from '../components/profile'
 import styles from '../styles/Home.module.scss'
 import { 
-  Box, 
-  Heading, 
+  Box,
+  Text,
+  Heading,
+  Image,
   Container,
   useColorModeValue 
 } from '@chakra-ui/react'
@@ -20,6 +22,8 @@ export default function Home() {
         <Alert mb={6}>
           Hello, I&apos;m a front-end developer based in Moscow!
         </Alert>
+
+        <Profile />
 
         <Box>
           <Heading as="h2" color="spacejelly">Chakra`s UI heading</Heading>
